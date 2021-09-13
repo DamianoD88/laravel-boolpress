ae63d70e2f581f72afb2cbd7d7f0699f1c406813
DamianoD88 <dispe88@live.it>
2021-09-13 22:51:43 +0200

    add slug

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,6 +13,17 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 0; $i < 10; $i++){
+            //creare l'istanza
+            $newPost = new Post();
+            //generare i dati
+            $newPost->title = 'Post numero ' . ($i + 1);
+            $newPost->slug = Str::slug($newPost->title, '-');
+            $newPost->content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, iure asperiores, aliquid accusamus dolores corporis nisi quaerat vel fugit soluta atque illum nostrum amet recusandae laudantium? Voluptates commodi quas sed!';
+
+            //salvare i dati
+            $newPost->save();
+
+        }
     }
 }
